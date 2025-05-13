0f8779756ffcf26c22c2463a85bb064cf7db4704
hxn2k3SGU <hoctapcunghxn@gmail.com>
2025-05-14 01:16:15 +0700

    commitaddtodo

---
+++ todo_app/src/components/Todo.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+function Todo(){
+    // Phần xử lý logic
+    const [todo, setTodo] = useState({
+        id: "",
+        name: "",
+    });
+
+    const [todoList, setTodoList] = useState([{
+        id: 1,
+        name: "Đánh răng rửa mặt",
+    },
+    {
+        id: 2,
+        name: "Đi học",    }]);
+
+    // Phần xử lý sự kiện 
+    const handleGetTodo = (value) => {
+        console.log(value);
+        const randomID = Math.floor(Math.random(10000) * 10000)
+
+        const todo = {
+            id: randomID,
+            name: value,
+        }
+        // console.log(todo);
+        // Nghĩa là mỗi lần onChange thì cái biến todo sẽ được cập nhật 
+        setTodo(todo);
+    }
+
+    const handleAddTodo = () => {
+        setTodoList((prev) => {
+            // console.log(1);
+            return [...prev, todo]
+        })
+        setTodo(
+            {
+                id: "",
+                name: "",
+            }
+        )
+    }
+
+    return(
+        <div>
+            <div>
+                <input value={todo.name} 
+                onChange={(e) => handleGetTodo(e.target.value)} 
+                placeholder="Nhập nhiệm vụ.." style={{
+                    width: "300px",
+                    height: "30px" }}/>
+                    
+                <br />    
+                <button onClick={handleAddTodo} 
+                style={
+                    {
+                        width: "100px",
+                        height: "30px",
+                        backgroundColor: "blue",
+                        color: "white",
+                        borderRadius: "5px",
+                        marginTop: "10px",
+                    }
+                }>ADD</button>
+                
+            </div>
+            <div>
+                <ul>
+                    {todoList.map((todo, index)=>{
+                        // return <li> {index}{todo.id} {todo.name} </li>
+                        return <li>{todo.name}</li>
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
+}
+export default Todo;
+// Giải thích phần add xong thì nó mất dữ liệu trong input
+// Đầu tiên là chúng ta cần set giá trị cho input mặc định là todo.name
+// Sau khi onChange thì dữ liệu của thằng todo được cập nhật liên tục
+// Nên là sau khi add xong thì chúng ta cần set lại tất cả thuộc tính của todo về mặc định
+// Set ở phàn addTodo
